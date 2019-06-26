ac512c4c0e8124f3016d9ff90386595cea561d41
chandeepadissanayake@gmail.com <Chandee1>
2019-06-26 16:18:50 +0530

    Modified tests to suit CMUSphinx Speech Recognition.

---
+++ EmulationCore/tests/utils/args_utils.py
@@ -0,0 +1,23 @@
+import argparse
+
+def get_cli_args(arg_keys: list):
+    parser = argparse.ArgumentParser()
+
+    for arg_key in arg_keys:
+        parser.add_argument("--" + arg_key)
+
+    return vars(parser.parse_args())
+
+def check_cli_args(check_arg_keys: list):
+    dict_args = get_cli_args(check_arg_keys)
+    exist_dict = {}
+    for arg in check_arg_keys:
+        exist_dict[arg] = dict_args[arg] is not None
+
+    return exist_dict
+
+def add_cli_args(args_help_dict: dict):
+    parser = argparse.ArgumentParser()
+
+    for arg, help_str in args_help_dict.items():
+        parser.add_argument(arg, help=help_str, action='store_true')
